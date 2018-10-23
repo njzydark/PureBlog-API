79fd150087552d271bd01af8fa31518d7f114025
njzy <1120897196@qq.com>
2018-10-23 20:55:31 +0800

    fix: 调整中间件使用逻辑并导入博文路由

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const controller = require('../controllers/users')
+const validateToken = require('../middlewares/validateToken')
+const isAdmin = require('../middlewares/isAdmin')
 
-router.get('/', controller.getAllUsers)
-router.get('/:id', controller.getUserById)
-router.delete('/:id', controller.deleteUserById)
+router.get('/', validateToken, isAdmin, controller.getAllUsers)
+router.get('/:id', validateToken, isAdmin, controller.getUserById)
+router.delete('/:id', validateToken, isAdmin, controller.deleteUserById)
 
 module.exports = router
--- routes/v1.js
+++ routes/v1.js
@@ -1,16 +1,14 @@
 const router = require('express').Router()
 // 用户信息相关路由
 const users = require('./users')
-
-// 验证token合法性
-const validateToken = require('../middlewares/validateToken')
-// 是否具有admin权限
-const isAdmin = require('../middlewares/isAdmin')
+// 博文信息相关路由
+const blogs = require('./blogs')
 // 用户信息相关操作
 const usersController = require('../controllers/users')
 
 router.post('/login', usersController.login)
 router.post('/register', usersController.register)
-router.use('/users', validateToken, isAdmin, users)
+router.use('/users', users)
+router.use('/blogs', blogs)
 
 module.exports = router
