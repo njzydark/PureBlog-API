a5e75e26184d05f3b9b52018e61fe6fdfe4565ad
njzy <1120897196@qq.com>
2018-10-24 16:38:37 +0800

    feat: 创建博文基础功能完成

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,7 +8,35 @@ module.exports = {
     res.send('get blog by id ' + req.params.id)
   },
   async createBlog(req, res, next) {
-    res.send('create blog')
+    const payload = req.decoded
+    const {
+      title,
+      content,
+      categoryId,
+      tagsId
+    } = req.body
+    if (!title || !content) {
+      return res.status(400).send({
+        success: false,
+        message: '标题或正文不能为空'
+      })
+    }
+    try {
+      const blog = new Blog({
+        authorId: payload.id,
+        title,
+        content,
+        categoryId,
+        tagsId
+      })
+      const result = await blog.save()
+      res.status(201).send({
+        success: true,
+        data: result
+      })
+    } catch (e) {
+      next(e)
+    }
   },
   async updateBlogById(req, res, next) {
     res.send('update blog by id ' + req.params.id)
