3b2f4f554084b11acd924a9ed9378c31d5488c4d
njzy <1120897196@qq.com>
2019-02-22 15:13:56 +0800

    docs: 更新依赖 git忽略项和eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,17 @@
 module.exports = {
   root: true,
-  extends: ['standard', 'prettier', 'prettier/standard'],
-  plugins: ['standard', 'prettier'],
-  rules: {
-    'prettier/prettier': ['error', { semi: false, singleQuote: true }],
-    eqeqeq: ['off']
-  },
   parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
   env: {
     es6: true,
     node: true
+  },
+  extends: ['standard', 'prettier', 'prettier/standard'],
+  plugins: ['standard', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { semi: false, singleQuote: true, printWidth: 120 }],
+    eqeqeq: ['off']
   }
 }
